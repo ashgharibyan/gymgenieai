7b6c0f7debf5eb05997ddff85a78f9980fa59f56
Ash Gharibyan <ashghcode@gmail.com>
2024-06-13 16:52:30 -0700

    added settings page

--- src/app/(signed-in)/settings/page.tsx
+++ src/app/(signed-in)/settings/page.tsx
@@ -1,6 +1,10 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
 
 export default async function Page() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log("------------", user);
   return (
     <div>
       <h1>Settings </h1>
