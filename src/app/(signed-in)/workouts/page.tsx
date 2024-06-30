4847de466e6ad2d4fc6f378fe634cdf3dbb6f8ed
gymgenieai2024 <gymgenieai@gmail.com>
2024-06-29 21:10:31 -0700

    added openai, generating workout json (#11)
    * added openai, generating workout json
    
    
    ---------
    
    Co-authored-by: Ash Gharibyan <ashghcode@gmail.com>

--- src/app/(signed-in)/workouts/page.tsx
+++ src/app/(signed-in)/workouts/page.tsx
@@ -1,6 +1,29 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
+import { api } from "~/trpc/server";
 
 export default async function Page() {
+  const { getUser } = getKindeServerSession();
+  const kindeUser = await getUser();
+  const user =
+    kindeUser != null
+      ? await api.user.getByIdWithProfile({ id: kindeUser.id })
+      : undefined;
+
+  const profile = user?.profile ?? undefined;
+  const goal = profile
+    ? await api.goal.getByProfileId({ profileId: profile?.id })
+    : undefined;
+
+  const workouts =
+    profile && goal
+      ? await api.openai.generateWorkout({
+          profile: profile,
+          goal: goal,
+        })
+      : undefined;
+
+  console.log(workouts);
   return (
     <div>
       <h1>Workouts </h1>
