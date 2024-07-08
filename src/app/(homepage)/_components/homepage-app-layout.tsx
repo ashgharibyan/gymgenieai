1c915e0c4d73f90ab56593f7b42585e9c35143bd
Ash Gharibyan <ashghcode@gmail.com>
2024-07-08 14:23:56 -0700

    fix err

--- src/app/(homepage)/_components/homepage-app-layout.tsx
+++ src/app/(homepage)/_components/homepage-app-layout.tsx
@@ -3,9 +3,7 @@
 import { AppShell } from "@mantine/core";
 import { Header } from "./header";
 import { Footer } from "./footer";
-import { useScrollIntoView } from "@mantine/hooks";
-import React, { MutableRefObject } from "react";
-import { useScrollContext } from "~/app/_context/ScrollContext";
+import React from "react";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
